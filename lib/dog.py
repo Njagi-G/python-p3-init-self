45ed330afd8cbfffb22ee41862c2e77eceb8d716
Njagi Githaiga <njagigithaiga@gmail.com>
2024-06-02 17:09:21 +0300

    Add attributs to class Dog and class Person

--- lib/dog.py
+++ lib/dog.py
@@ -2,3 +2,17 @@
 
 class Dog:
     pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+    
+beast = Dog("Beast", "Alsatian")
+
+beast.name
+print(beast.name)
+
+beast.breed
+print(beast.breed)
+
+
--- lib/person.py
+++ lib/person.py
@@ -2,3 +2,12 @@
 
 class Person:
     pass
+    
+    def __init__(self, name):
+        self.name = name
+
+
+peggy_lee = Person("Peggy Lee")
+
+peggy_lee.name
+print(peggy_lee.name)
